0b8a8ad0e2b8e665d2a39d9ff1860ce8a923c683
Nick Yahnke <nthitz@gmail.com>
2015-03-10 21:50:09 -0700

    iconv notes to convert to utf8, not yet implmented

---
+++ scripts/convertToUTF8.js
@@ -0,0 +1,4 @@
+// TODO something along these lines
+// iconv -f ISO-8859-1 -t UTF-8 imdb_data/actors.list >> imdb_data/actors.utf8.lis
+// see also https://github.com/bnoordhuis/node-iconv
+console.log('TODO')
